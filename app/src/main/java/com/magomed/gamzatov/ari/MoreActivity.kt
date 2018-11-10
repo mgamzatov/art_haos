fb712acef933dbd85153c838502f663e0543eb8e
mgamzatov <gamz360@gmail.com>
2018-11-10 07:27:10 +0300

    music player

--- app/src/main/java/com/magomed/gamzatov/ari/MoreActivity.kt
+++ app/src/main/java/com/magomed/gamzatov/ari/MoreActivity.kt
@@ -1,12 +1,38 @@
 package com.magomed.gamzatov.ari
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageButton
 
 class MoreActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.more)
+
+        mp = MediaPlayer.create (this, R.raw.lebedi)
+        var position = 0
+
+        val playButton = findViewById<ImageButton>(R.id.play_button)
+
+        playButton.setOnClickListener {
+            if (mp.isPlaying) {
+                playButton.setBackgroundResource(R.drawable.ic_play_circle)
+                position = mp.currentPosition
+                mp.pause()
+            } else {
+                playButton.setBackgroundResource(R.drawable.ic_pause_circle)
+                mp.seekTo(position)
+                mp.start()
+            }
+        }
+    }
+
+    override fun onDestroy () {
+        super.onDestroy ()
+        mp.release ()
     }
 }
